a9628f1d445ed2377fe18e1c014531c99d6f221a
oscar.segarra <oscar.segarra@est.fib.upc.edu>
2017-11-16 11:34:13 +0100

    Actualització

--- src/Drivers/DriverMasterMind.java
+++ src/Drivers/DriverMasterMind.java
@@ -130,13 +130,13 @@ public class DriverMasterMind {
                             try {
                                 i = sc.nextInt();
                                 if (i <= 0 || i > rondes) throw new IllegalArgumentException();
-                                bucle3 = false;
+                                else bucle3 = false;
                             } catch (InputMismatchException e) {
                                 System.out.println("El paràmetre introduït no és un nombre");
                                 sc.nextLine();
                             } catch (IllegalArgumentException e) {
                                 if (i <= 0) System.out.println("El nombre introduït no és vàlid, siusplau introdueix un enter positiu");
-                                else System.out.println("El nombre introduït és major que el nombre de rondes màxim de la partida");
+                                else System.out.println("El nombre introduït és major que el nombre de rondes màxim de la partida. Torna a introduïr el nombre de rondes jugades");
                             }
                         }
                         masterMind.guardarPartida(jugador, i);
