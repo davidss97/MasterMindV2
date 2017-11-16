dff04f136b61af485953c391bf1af58d805a2d93
oscar.segarra <oscar.segarra@est.fib.upc.edu>
2017-11-16 15:58:54 +0100

    Actualització

--- src/Domini/CodeB.java
+++ src/Domini/CodeB.java
@@ -5,7 +5,7 @@ import java.util.Vector;
 public class CodeB extends Rol{
     public void enviarIntent(Vector<Peca> comb) {
         System.out.println("Enviant intent...");
-        if (! P.codiExisteix()) System.out.println("No hi ha cap codi introduit");
+        if (! P.existeixCodi()) System.out.println("No hi ha cap codi introduit");
         else{
             P.comprobarCodi(comb);
         }
