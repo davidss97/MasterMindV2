62e9382a56bbb94b79f898962b354ca80e3603ff
oscar.segarra <oscar.segarra@est.fib.upc.edu>
2017-11-16 14:47:26 +0100

    Actualització

--- src/Dades/Classificacio.java
+++ src/Dades/Classificacio.java
@@ -1,27 +1,21 @@
 package Dades;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.LinkedList;
-
+//Se m'ha posat * automàticament
+import java.io.*;
+import java.util.*;
 
 //test
 public class Classificacio {
 
     private String nomJugador;
     private String rond;
+    private int rondInt;
 
     //Constructora
     public Classificacio(String nomJugador,int rondesFinals) {
         this.nomJugador = nomJugador;
         this.rond = String.valueOf(rondesFinals);
+        this.rondInt = rondesFinals;
     }
 
     //Funcions entrants:
@@ -35,13 +29,57 @@ public class Classificacio {
         guardarPartida("Dificil.txt");
     }
     private void guardarPartida(String path){
+        //PART 1 PER GUARDAR ORDENAT
+        File archivo;
+        FileWriter escribir;
+        FileReader leer;
+        PrintWriter pw;
+
+        BufferedReader br;
+        ArrayList<String> llista = new ArrayList<String>();
+
+        String linea, cadena;
+        Iterator iter;
+
         try {
-            File archivo = new File(path);
-            FileWriter escribir = new FileWriter(archivo, true);
-            escribir.write(rond + " " + nomJugador + "\n");
+            //crear o obrir .txt de la dificultat corresponent
+            archivo = new File(path);
+            escribir = new FileWriter(archivo, true);
+
+            leer = new FileReader(path);
+            br = new BufferedReader(leer);
+            //agafa contingut a la llista
+            if (rondInt < 10) llista.add("0" + rond + " " + nomJugador);
+            else llista.add(rond + " " + nomJugador);
+
+            while ((linea = br.readLine()) != null){ llista.add(linea); }
+
+            //ordena per strings, per tant error perque agafa 10 < 9
+            Collections.sort(llista);
+
+            pw = new PrintWriter(escribir);
+            //netejar/esborrar fitxer
+            new PrintWriter(path).close();
+
+            //escriu ja ordenat de la llista
+            iter = llista.iterator();
+            while (iter.hasNext())
+            {
+                cadena = (String) iter.next();
+                pw.println(cadena);
+            }
+
+            //tanquem
             escribir.close();
+            br.close();
+            pw.close();
+            llista.clear();
         }
-        catch (Exception e){
+        catch (FileNotFoundException e) {
+            System.err.println("No s'ha trobat el fitxer");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e){
             System.out.println("Error a l'escriure");
         }
     }
@@ -50,36 +88,31 @@ public class Classificacio {
     public void veureClassificacioMitjana (){ veureClassificacio("Mitjana.txt"); }
     public void veureClassificacioDificil (){ veureClassificacio("Dificil.txt"); }
     private void veureClassificacio (String path){
-        //PART 2 PER ORDENAR I MOSTRAR PER PANTALLA
-        BufferedReader br = null;
+        //PART 2 PER MOSTRAR PER PANTALLA
+        BufferedReader br;
         LinkedList<String> llista = new LinkedList<String>();
+        String cadena,linea;
+        Iterator iter;
         try {
 
             br = new BufferedReader(new FileReader(path));
-            FileWriter fw = new FileWriter(path);
-            PrintWriter pw = new PrintWriter(fw);
-            String linea = null;
             while ((linea = br.readLine()) != null){
                 llista.add(linea);
             }
-            Collections.sort(llista);
-            Iterator iter = llista.iterator();
-            String cadena;
+            iter = llista.iterator();
             while (iter.hasNext())
             {
                 cadena = (String) iter.next();
-                pw.println(cadena);
                 //MOSTRAR PER PANTALLA
                 System.out.println(cadena);
             }
             br.close();
-            pw.close();
-            fw.close();
         } catch (FileNotFoundException e) {
-            System.err.println("La classificació no ha sigut encara creada");
+            System.err.println("No s'ha trobat el fitxer");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
 
