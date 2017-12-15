5d2a6b3784c4567b6fc94e2186a9f5e8a92fd852
oscar.segarra <oscar.segarra@est.fib.upc.edu>
2017-12-15 09:17:33 +0100

    Intent 1

--- src/Presentacio/PantallaInici.java
+++ src/Presentacio/PantallaInici.java
@@ -1,4 +1,24 @@
 package Presentacio;
+import javax.swing.*;
+import java.awt.*;
 
 public class PantallaInici {
+    PantallaInici(){
+        JFrame frame = new JFrame("Panalla d'Inici Mastermind");
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        int xSize = ((int) tk.getScreenSize().getWidth());
+        int ySize = ((int) tk.getScreenSize().getHeight());
+        frame.setVisible(true);
+        frame.setSize(xSize/3*2, ySize/10*9);
+        JButton b1 = new JButton("1");
+        JButton b2 = new JButton("2");
+        JButton b3 = new JButton("3");
+        frame.add(b1);
+        frame.add(b2);
+        frame.add(b3);
+        frame.setLayout(new GridLayout(1,3));
+
+       // frame.setSize(500,800);
+        //frame.setExtendedState(JFrame.MAXIMIZED_BOTH); //si volem pantalla completa
+    }
 }
