97630d28e43b4ba7b93691a8e1b87d2ef8baa248
david <david.sanchez.soles@est.fib.upc.edu>
2017-12-18 13:43:17 +0100

    estructura partida

--- src/Presentacio/RoundButton.java
+++ src/Presentacio/RoundButton.java
@@ -5,19 +5,40 @@ import java.awt.RenderingHints;
 import javax.swing.*;
 
 public class RoundButton extends JButton {
-
-    public RoundButton(String label) {
-        super(label);
-
-        setBackground(new Color(0,255,0));
+    int tam;
+    public RoundButton(Boolean small, int c1, int c2, int c3) {
+        super("");
+        setBackground(new Color(c1,c2,c3));
         setFocusable(false);
-
+        tam = 20;
     /*
      These statements enlarge the button so that it
      becomes a circle rather than an oval.
     */
         Dimension size = getPreferredSize();
-        size.width = size.height = Math.max(size.width, size.height);
+        size.width = size.height = tam;//Math.max(50,50);//size.width, size.height);
+        setPreferredSize(size);
+
+    /*
+     This call causes the JButton not to paint the background.
+     This allows us to paint a round background.
+    */
+        setContentAreaFilled(false);
+    }
+
+
+    public RoundButton(int c1, int c2, int c3) {
+        super("");
+
+        setBackground(new Color(c1,c2,c3));
+        setFocusable(false);
+        tam = 50;
+    /*
+     These statements enlarge the button so that it
+     becomes a circle rather than an oval.
+    */
+        Dimension size = getPreferredSize();
+        size.width = size.height = tam;//Math.max(50,50);//size.width, size.height);
         setPreferredSize(size);
 
     /*
@@ -33,7 +54,7 @@ public class RoundButton extends JButton {
         } else {
             g.setColor(getBackground());
         }
-        g.fillOval(0, 0, getSize().width - 1, getSize().height - 1);
+        g.fillOval(0, 0, tam,tam);//getSize().width - 1, getSize().height - 1);
 
         super.paintComponent(g);
     }
@@ -42,7 +63,7 @@ public class RoundButton extends JButton {
         Graphics2D graphics2D = (Graphics2D)g;
         graphics2D.setColor(Color.darkGray);
         graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        graphics2D.drawOval(0, 0, getSize().width - 1, getSize().height - 1);
+        graphics2D.drawOval(0, 0, tam,tam);//getSize().width - 1, getSize().height - 1);
     }
 
     // Hit detection.
@@ -51,24 +72,9 @@ public class RoundButton extends JButton {
     public boolean contains(int x, int y) {
         // If the button has changed size,  make a new shape object.
         if (shape == null || !shape.getBounds().equals(getBounds())) {
-            shape = new Ellipse2D.Float(0, 0, getWidth(), getHeight());
+            shape = new Ellipse2D.Float(0, 0, tam,tam);//getWidth(), getHeight());
         }
         return shape.contains(x, y);
     }
 
-    public static void main(String[] args) {
-
-        JFrame frame = new JFrame("Rounded Button Example");
-        frame.setLayout(new FlowLayout());
-
-        JButton b1 = new RoundButton("B1");
-        JButton b2 = new RoundButton("B2");
-        b1.setBackground(new Color(255,0,0));
-
-        frame.add(b1);
-        frame.add(b2);
-
-        frame.setSize(1000, 1000);
-        frame.setVisible(true);
-    }
 }
