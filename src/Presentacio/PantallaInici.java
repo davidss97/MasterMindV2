a7f1952171c07f8528e1782f4be75b88160977d5
oscar.segarra <oscar.segarra@est.fib.upc.edu>
2017-12-18 12:33:35 +0100

    Pantalla Inici 18/12 - 12.33

--- src/Presentacio/PantallaInici.java
+++ src/Presentacio/PantallaInici.java
@@ -4,21 +4,25 @@ import java.awt.*;
 
 public class PantallaInici {
     PantallaInici(){
-        JFrame frame = new JFrame("Panalla d'Inici Mastermind");
+        JFrame frame = new JFrame("Pantalla d'Inici Mastermind");
         Toolkit tk = Toolkit.getDefaultToolkit();
         int xSize = ((int) tk.getScreenSize().getWidth());
         int ySize = ((int) tk.getScreenSize().getHeight());
+        frame.setSize(xSize/3*2, ySize/10*9); //En una 1920:1080 --> 1280:972
+        JPanel p1 = new JPanel();
+        JPanel p2 = new JPanel();
+        JPanel p3 = new JPanel();
+        p1.setBackground(Color.YELLOW);
+        p2.setBackground(Color.BLUE);
+        p3.setBackground(Color.RED);
+        p2.setSize(640,ySize/10*9);
+        frame.setLayout(new BorderLayout());
+        frame.add(p1,BorderLayout.WEST);
+        frame.add(p2,BorderLayout.CENTER);
+        frame.add(p3,BorderLayout.EAST);
         frame.setVisible(true);
-        frame.setSize(xSize/3*2, ySize/10*9);
-        JButton b1 = new JButton("1");
-        JButton b2 = new JButton("2");
-        JButton b3 = new JButton("3");
-        frame.add(b1);
-        frame.add(b2);
-        frame.add(b3);
-        frame.setLayout(new GridLayout(1,3));
 
-       // frame.setSize(500,800);
+        //frame.setSize(500,800);
         //frame.setExtendedState(JFrame.MAXIMIZED_BOTH); //si volem pantalla completa
     }
 }
