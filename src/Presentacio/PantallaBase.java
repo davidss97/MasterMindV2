c442277f007da20db561a0ee86000d0f874db0ea
oscar.segarra <oscar.segarra@est.fib.upc.edu>
2017-12-30 18:48:46 +0100

    Pantalla Base amb funció per canviar el centre

--- src/Presentacio/PantallaBase.java
+++ src/Presentacio/PantallaBase.java
@@ -4,8 +4,9 @@ import javax.swing.*;
 import java.awt.*;
 
 public class PantallaBase {
+    private JFrame frame = new JFrame("Mastermind");
+    private GridBagConstraints c = new GridBagConstraints();
     PantallaBase(){
-        JFrame frame = new JFrame("Mastermind");
         Toolkit tk = Toolkit.getDefaultToolkit();
         int xSize = ((int) tk.getScreenSize().getWidth());
         int ySize = ((int) tk.getScreenSize().getHeight());
@@ -15,10 +16,8 @@ public class PantallaBase {
         pleft.setBackground(Color.BLUE);
         pright.setBackground(Color.RED);
         pleft.setMinimumSize(new Dimension(xSize/3*2/4,ySize/10*9));
-        PantallaInici center = new PantallaInici();
         pright.setMinimumSize(new Dimension(xSize/3*2/4,ySize/10*9));
         frame.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
         c.fill = GridBagConstraints.BOTH;
         c.gridwidth = 1;
         c.weightx = 0.4;
@@ -26,11 +25,7 @@ public class PantallaBase {
         c.gridx = 0;
         c.gridy = 0;
         frame.add(pleft, c);
-        c.gridx = 1;
-        c.gridy = 0;
-        c.gridwidth = 2;
-        c.weightx = 1;
-        frame.add(center, c);
+        changeCenter(new PantallaInici());
         c.weightx = 0.4;
         c.gridwidth = 1;
         c.gridx = 3;
@@ -38,8 +33,15 @@ public class PantallaBase {
         frame.add(pright, c);
 
         frame.setVisible(true);
-        center.setVisible(true);
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public void changeCenter(JPanel center){
+        c.gridx = 1;
+        c.gridy = 0;
+        c.gridwidth = 2;
+        c.weightx = 1;
+        frame.add(center, c);
     }
 }
