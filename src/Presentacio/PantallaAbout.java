803f547829a1d381d96091fb4783fc89324c09dd
oscar.segarra <oscar.segarra@est.fib.upc.edu>
2018-01-05 10:37:05 +0100

    Final 1

---
+++ src/Presentacio/PantallaAbout.java
@@ -0,0 +1,13 @@
+package Presentacio;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class PantallaAbout extends JPanel{
+    private PantallaBase base;
+    PantallaAbout(PantallaBase base){
+        this.base = base;
+        super.setBackground(new Color(250, 50, 123));
+    }
+}
