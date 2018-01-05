c44c12416afd40260e58319fd7ef93bb374100c8
oscar.segarra <oscar.segarra@est.fib.upc.edu>
2018-01-05 10:37:17 +0100

    Final 1

---
+++ src/Presentacio/PantallaRules.java
@@ -0,0 +1,12 @@
+package Presentacio;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PantallaRules extends JPanel{
+    private PantallaBase base;
+    PantallaRules(PantallaBase base){
+        this.base = base;
+        super.setBackground (new Color(40, 40, 40));
+    }
+}
