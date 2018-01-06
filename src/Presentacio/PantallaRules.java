6c9a5587d661f18fb2d2f2ed89c17a0356f6bc53
oscar.segarra <oscar.segarra@est.fib.upc.edu>
2018-01-06 22:21:17 +0100

    Final 1

--- src/Presentacio/PantallaRules.java
+++ src/Presentacio/PantallaRules.java
@@ -15,7 +15,7 @@ public class PantallaRules extends JPanel{
                 "<li>The objective of the game is to guess the exact positions of the colors in the computer's sequence." +
                 "<li>By default, a color can be used only once in a code sequence. If you start a new game with the 'Allow duplicates' checked, then any color can be used any number of times in the code sequence." +
                 "<li>After filling a line with your guesses and clicking on the 'Check' button, the computer responses with the result of your guess." +
-                "<li>For each color in your guess that is in the correct color and correct position in the code sequence, the computer display a small red color on the right side of the current guess." +
+                "<li>For each color in your guess that is in the correct color and correct position in the code sequence, the computer display a small black color on the right side of the current guess." +
                 "<li>For each color in your guess that is in the correct color but is NOT in the correct position in the code sequence, the computer display a small white color on the right side of the current guess." +
                 "<li>You win the game when you manage to guess all the colors in the code sequence and when they all in the right position." +
                 "<li>You lose the game if you use all attempts without guessing the computer code sequence.<br> You lose the game if you use all attempts without guessing the computer code sequence.</ul>");
